40117f7e8fe5eddb470f3c9eb546534e71531803
Eric Ostler <eostler13@gmail.com>
2018-02-12 21:02:11 -0700

    added js Variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Variables and Arrays
+
+
+var wordList = ["", "", "", "", "",]; 
+var computerPick = ""; 
+var wordsLetters = [];
+var numBlanks = 0;
+var blanksAndSuccess = [];
+var wrongLetters = [];
+
