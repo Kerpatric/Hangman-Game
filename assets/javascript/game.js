3f945a0f379c19105dbcccbce2a9a71545c8a41b
Eric Ostler <eostler13@gmail.com>
2018-02-12 21:45:40 -0700

    added win/loss conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // Variables and Arrays
 
 
-var wordList = ["", "", "", "", "",]; 
-var computerPick = ""; 
+var wordList = ["", "", "", "", "",];
+var computerPick = "";
 var wordsLetters = [];
 var numBlanks = 0;
 var blanksAndSuccess = [];
@@ -42,21 +42,67 @@ function start() {
 function compareLetters(letter) {
     var isLetterInWord = false;
 
-    for (var i=0; i<numBlanks; i++){
-        if(computerPick[i] == letter) {
+    for (var i = 0; i < numBlanks; i++) {
+        if (computerPick[i] == letter) {
             isLetterInWord = true;
         }
     }
+
+    //find where the letter exists, then fills in blanksAndSuccesses array.
+    if (isLetterInWord) {
+        for (var i = 0; i < numBlanks; i++) {
+            if (computerPick[i] == letter) {
+                blanksAndSuccess[i] = letter;
+            }
+        }
+
+    }
+
+    else {
+        wrongLetters.push(letter);
+        guessesRemain--
+    }
 }
 
-// Main Process
+function roundFinish(){
+    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left" + guessesRemain);
 
-// Initiates code for the first time
-start();
+    // Updates HTML to show most current stats
+    document.getElementById("guessLeft").innerHTML = guessesRemain;
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccess.join(" ");
+    document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
 
-// registers the users key presses
-document.onkeyup = function(event) {
-    var letterPicked = String.fromCharCode(event.keyCode).toLowerCase();
-    checkLetters(letterPicked);
+    //Check if user wins
+    if (wordsLetters.toString() == blanksAndSuccess.toString()) {
+        winCount++;
+        alert("YOU WIN!");
 
+        // updates the win counter in HTML
+        document.getElementById("wins").innerHTML = winCount;
+
+        start();
+    }
+    //Check if user loses
+    else if (guessesRemain == 0) {
+        lossCount++;
+        alert("YOU LOSE!");
+
+        //updates the loss counter in HTML
+        document.getElementById("losses").innerHTML = lossCount;
+
+        start();
+    }
 }
+
+    // Main Process
+
+    // Initiates code for the first time
+    start();
+
+    // registers the users key presses
+    document.onkeyup = function (event) {
+        var letterPicked = String.fromCharCode(event.keyCode).toLowerCase();
+        checkLetters(letterPicked);
+        roundFinish();
+
+    }
